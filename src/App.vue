394d8c15207c1181a92542f706dd00eda1fb49c8
Matt Lissner <matt@doejo.com>
2018-02-16 17:03:15 -0600

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <ul class="main-nav">
+      <li class="main-nav__item" ref="mainnavitem" v-for="(item, index) in items" :key="index" v-html="item.message" :data-postIndex="index+2" v-on:click="bounce">
+      </li>
+    </ul>
+    <div class="ssl-warning" v-bind:class="{ active: isActive }">
+      <p>Sorry, I didn't want to pay godaddy $75 for SSL so you'll need to allow your browser from unauthenticated sources</p>
+    </div>
+    <div class="col-xs-12 text-center info">
+      <transition name="slide-fade">
+        <h1 class="init-header" v-if="show">{{ introMessage }}</h1>
+      </transition>
+    </div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -8,11 +20,53 @@
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+
 export default {
   name: 'app',
   components: {
     HelloWorld
-  }
+  },
+  data: function() {
+    return{
+      items: [],
+      isActive: false,
+      show: true,
+      introMessage:  'Hiyo'
+    }
+  },
+  mounted: function(){
+    this.$http.get('http://matthewlissner.com/wp-json/wp/v2/hot_sauces?filter[orderby]=date&order=asc').then(response => {
+      let x = this;
+      response.body.forEach(function(val){
+        x.items.push({message: val.title.rendered})
+      })
+    }, () => {
+      console.log('sorry about that');
+      this.isActive = true;
+      // error callback
+    });
+  },
+  methods: {
+    begin: function () {
+      setTimeout(() => {
+        let elements = document.getElementsByClassName('main-nav__item');
+        elements[0].classList.add('main-nav__item--bouncing');
+        console.log(elements[0].innerHTML);
+        this.introMessage = this.items[0].message;
+      }, 1000);
+    },
+    bounce: function (event) {
+      this.$refs.mainnavitem.forEach(function(val){
+        val.classList.remove('main-nav__item--bouncing');
+      })
+      event.target.classList.add('main-nav__item--bouncing');
+      this.introMessage = event.target.innerHTML;
+    }
+  },
+  updated(){
+     this.begin()
+  },
+
 }
 </script>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,7 +13,7 @@
     </ul>
     <h3>Essential Links</h3>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a class="yellow-chris" href="https://vuejs.org" target="_blank">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
       <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
       <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
@@ -53,5 +53,8 @@ li {
 }
 a {
   color: #42b983;
+}
+.yellow-chris {
+  color:red;
 }
 </style>
